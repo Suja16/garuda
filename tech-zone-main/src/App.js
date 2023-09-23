96acb4acfdc7459bafe0695302f6ae2ff98873b2
Suja16 <sujal16@somaiya.edu>
2023-09-24 02:35:52 +0530

    feat : search bar

--- tech-zone-main/src/App.js
+++ tech-zone-main/src/App.js
@@ -1,20 +1,27 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-
 import Slider from "./components/Slider";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import SearchBar from "./components/SearchBar"; // Import the SearchBar component
 
 function App() {
+  // Define a function to handle search
+  const handleSearch = (query) => {
+    // You can add your API request logic here to search for products
+    console.log("Searching for:", query);
+  };
+
   return (
     <>
       <div className="app min-h-screen bg-gray-50 text-gray-700">
         <Navbar />
+        <SearchBar onSearch={handleSearch} /> {/* Render the SearchBar */}
         <ToastContainer />
         <Routes>
           <Route path="/" element={<Home />} />
