0f76e56b7b45acc7d9f976f1d59a7b9c637c4e42
Aditya-A-Chavan <adityachavan271@gmail.com>
2023-10-09 00:04:13 +0530

    refined banning

--- material-dashboard-react-main/src/components/ProductForm.js
+++ material-dashboard-react-main/src/components/ProductForm.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import MDBox from "components/MDBox";
 import './ConsignmentForm.css';
 
+
+
 const ConsignmentForm = () => {
   const [productTitle, setProductTitle] = useState('');
   const [productSKU, setProductSKU] = useState('');
@@ -11,21 +13,23 @@ const ConsignmentForm = () => {
   const [productCategory, setProductCategory] = useState('');
   const [productImage, setProductImage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const [blockedKeywords, setBlockedKeywords] = useState([]);
+  /* const [blockedKeywords, setBlockedKeywords] = useState([]); */
 
-  useEffect(() => {
+/*   useEffect(() => {
     
     fetch('/blocked-keywords.txt')
       .then((response) => response.text())
       .then((text) => {
-        
-        const keywords = text.split('\n');
+      
+        const keywords = text.split(/\r\n|\r|\n/).filter((keyword) => keyword.trim() !== '');
         setBlockedKeywords(keywords);
       })
       .catch((error) => {
         console.error('Error loading blocked keywords:', error);
       });
-  }, []);
+  }, []); */
+
+  const blockedKeywords = ['CATHINONE', 'DOB', 'BROLAMFETAMINE', 'DET', 'DMA', 'DMHP', 'DMT', 'DOET', 'ETICYCLIDINE', 'PCE', 'ETRYPTAMINE', 'N-hydroxy MDA', '(+)-LYSERGIDE', 'LSD, LSD-25', 'MDE, N-ethyl MDA', 'MDMA', 'mescaline', 'methcathinone', '4-methylaminorex', 'MMDA', '4-MTA', 'parahexyl', 'PMA', 'psilocine, psilotsin', 'PSILOCYBINE', 'ROLICYCLIDINE', 'PHP', 'PCPY', 'STP', 'DOM', 'TENAMFETAMINE', 'MDA', 'TENOCYCLIDINE', 'TCP', 'TMA', 'AMFETAMINE', 'AMINEPTINE', 'DEXAMFETAMINE', 'DRONABINOLa', 'FENETYLLINE', 'LEVAMFETAMINE', 'MECLOQUALONE', 'METAMFETAMINE', 'METAMFETAMINE RACEMATE', 'METHAQUALONE', 'METHYLPHENIDATE', 'PHENCYCLIDINE', 'amphetamine', '2C-B', 'dexamphetamine', 'delta-9-tetrahydro- cannabinol and its stereochemical variants', 'levamphetamine', 'levomethamphetamine', 'methamphetamine', 'methamphetamine racemate', 'PCP', 'PHENMETRAZINE', 'SECOBARBITAL', 'ZIPEPROL', 'AMOBARBITAL', 'BUPRENORPHINE', 'BUTALBITAL', 'CATHINE', 'CYCLOBARBITAL', 'FLUNITRAZEPAM', 'GLUTETHIMIDE', 'PENTAZOCINE', 'PENTOBARBITAL', '(+)-norpseudoephedrine', 'ALLOBARBITAL', 'ALPRAZOLAM', 'AMFEPRAMONE', 'AMINOREX', 'BARBITAL', 'BENZFETAMINE', 'BROMAZEPAM', 'BROTIZOLAM', 'CAMAZEPAM', 'CHLORDIAZEPOXIDE', 'CLOBAZAM', 'CLONAZEPAM', 'CLORAZEPATE', 'CLOTIAZEPAM', 'CLOXAZOLAM', 'DELORAZEPAM', 'DIAZEPAM', 'ESTAZOLAM', 'ETHCHLORVYNOL', 'ETHINAMATE', 'ETHYL LOFLAZEPATE', 'ETILAMFETAMINE', 'FENCAMFAMIN', 'FENPROPOREX', 'FLUDIAZEPAM', 'FLURAZEPAM', 'HALAZEPAM', 'HALOXAZOLAM', 'KETAZOLAM', 'LEFETAMINE', 'LOPRAZOLAM', 'diethylpropion', 'benzphetamine', 'butobarbital', 'N-ethylamphetamine', 'GHB', 'SPA', 'LORAZEPAM', 'LORMETAZEPAM', 'MAZINDOL', 'MEDAZEPAM', 'MEFENOREX', 'MEPROBAMATE', 'MESOCARB', 'METHYLPHENOBARBITAL', 'METHYPRYLON', 'MIDAZOLAM', 'NIMETAZEPAM', 'NITRAZEPAM', 'NORDAZEPAM', 'OXAZEPAM', 'OXAZOLAM', 'PEMOLINE', 'PHENDIMETRAZINE', 'PHENOBARBITAL', 'PHENTERMINE', 'PINAZEPAM', 'PIPRADROL', 'PRAZEPAM', 'PYROVALERONE', 'SECBUTABARBITAL', 'TEMAZEPAM', 'TETRAZEPAM', 'TRIAZOLAM', 'VINYLBITAL', 'ZOLPIDEM', 'Allobarbital', 'Amfepramone', 'Amfetamine', 'Amineptine', 'Aminorex', 'Amobarbital', 'Barbital', 'Benzfetamine', 'Brolamfetamine (DOB)', 'Buprenorphine', 'Cathine', 'Chlordiazepoxide', 'Clorazepate', 'Cyclobarbital', 'DET', 'Dexamfetamine', 'Aminophenazone', 'Glutamate', 'Hydrochloride', 'Resinate', 'Acetylsalicylate', 'Adipate', 'Aspartate', 'Bitartrate', 'Hydrochloride', 'Para-aminophencylacetate', 'Parachlorophenoxyacetate', 'Pentobarbiturate', 'Phosphate (1 mol. base)', 'Phosphate (2 mol. base) Resinate', 'Sulfate (2 mol. base)', 'Tannate'];
 
   const handleProductTitleChange = (event) => {
     setProductTitle(event.target.value);
@@ -57,16 +61,15 @@ const ConsignmentForm = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
-   
+  
+    // Check if the description contains blocked keywords
     const containsBlockedKeyword = blockedKeywords.some((keyword) =>
-      productDescription.toLowerCase().includes(keyword.toLowerCase())
+      productDescription.toLowerCase().includes(keyword.trim().toLowerCase())
     );
-
+  
     if (containsBlockedKeyword) {
-      setErrorMessage('Contains Blocked Keyword. Please Make sure you are trying to sell a product that is allowed to sell');
+      setErrorMessage('Contains Blocked Keyword. Please make sure you are trying to sell a product that is allowed.');
     } else {
-      
       setProductTitle('');
       setProductSKU('');
       setProductDescription('');
