baabb9ad76335413b304fa536204add40be02d0a
RudraShinde75 <rudra.shinde@somaiya.edu>
2023-10-15 21:20:30 +0530

    Landing page updated

--- tech-zone-main/src/components/Navbar.js
+++ tech-zone-main/src/components/Navbar.js
@@ -1,63 +1,137 @@
-import { Link, NavLink } from "react-router-dom";
-import { BsCart3 } from "react-icons/bs";
-import { useSelector } from "react-redux";
-import SearchBar from "./SearchBar";
+// import { Link, NavLink } from "react-router-dom";
+// import { BsCart3 } from "react-icons/bs";
+// import { useSelector } from "react-redux";
+// import SearchBar from "./SearchBar";
+
+// const Navbar = () => {
+//   const { cartItems } = useSelector((state) => state.carts);
+//   const NavActive = ({ isActive }) => {
+//     return {
+//       color: isActive ? "#f9fafb" : null,
+//     };
+//   };
+
+//   return (
+//     <div className="navbar bg-gray-700 text-violet-50 h-20 flex items-center justify-center">
+//       <nav className="container mx-auto px-10  sm:flex-row flex-col  flex justify-between items-center">
+//         <div className="nav-left">
+//           <Link to={"/"}>
+//             <h2 className="brand text-2xl text-violet-50 italic font-semibold  ">
+//               <span className="text-rose-500">Garuda</span>
+//             </h2>
+//           </Link>
+//         </div>
+//         <SearchBar/>
+//         <div className="nav-right">
+//           <ul className="nav-links flex gap-5 items-center">
+//             <li>
+//               <NavLink
+//                 className="text-gray-300 font-normal hover:text-gray-400 duration-300"
+//                 style={NavActive}
+//                 end
+//                 to="/"
+//               >
+//                 Home
+//               </NavLink>
+//             </li>
+//             <li>
+//               <NavLink
+//                 className="text-gray-300 font-normal hover:text-gray-400 duration-300"
+//                 style={NavActive}
+//                 to={"/products"}
+//               >
+//                 Products
+//               </NavLink>
+//             </li>
+//             <li>
+//               <NavLink to="/cart" className="relative group">
+//                 <span>
+//                   <BsCart3 />
+//                   <span className=" absolute bottom-2 text-sm left-2 bg-rose-500 h-5 w-5 flex items-center justify-center rounded-full group-hover:bg-sky-500 duration-300">
+//                     {cartItems.length}
+//                   </span>
+//                 </span>
+//               </NavLink>
+//             </li>
+//           </ul>
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+import React from "react";
+import { FaSearch, FaBell } from "react-icons/fa";
 
 const Navbar = () => {
-  const { cartItems } = useSelector((state) => state.carts);
-  const NavActive = ({ isActive }) => {
-    return {
-      color: isActive ? "#f9fafb" : null,
-    };
-  };
-
   return (
-    <div className="navbar bg-gray-700 text-violet-50 h-20 flex items-center justify-center">
-      <nav className="container mx-auto px-10  sm:flex-row flex-col  flex justify-between items-center">
-        <div className="nav-left">
-          <Link to={"/"}>
-            <h2 className="brand text-2xl text-violet-50 italic font-semibold  ">
+    <div>
+      <div className="navbar bg-blue-700 text-violet-50 h-20 flex items-center justify-center">
+        <nav className="container mx-auto px-10 sm:flex-row flex-col flex justify-between items-center">
+          <div className="nav-left">
+            <h2 className="brand text-2xl text-violet-50 italic font-semibold">
               <span className="text-rose-500">Garuda</span>
             </h2>
-          </Link>
+          </div>
+          <div className="nav-center">
+            <div
+              className="search-bar bg-white flex items-center px-4 rounded-md"
+              style={{ width: "617px", height: "45px" }}
+            >
+              <input
+                type="text"
+                className="w-full h-full outline-none"
+                placeholder="Search..."
+              />
+              <span className="ml-2 text-gray-500">
+                <FaSearch />
+              </span>
+            </div>
+          </div>
+          <div className="nav-right">
+            <ul className="nav-links flex gap-5 items-center">
+              <li>
+                <a
+                  href="/signup"
+                  className="text-gray-300 font-normal hover:text-gray-400 duration-300"
+                >
+                  Sign Up
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/login"
+                  className="text-gray-300 font-normal hover:text-gray-400 duration-300"
+                >
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+
+      <div className="bg-blue-700 text-white py-2 text-center flex justify-start">
+        <span className="text-xl font-semibold ml-auto" style={{ marginLeft: "150px" }}>
+          Categories
+        </span>
+        <div className="ml-auto flex items-center" style={{ marginRight: "150px" }}>
+          <span className="text-xl font-semibold">Features & Benefits</span>
+          <span className="mx-4 text-gray-500">|</span>
+          <span className="text-xl font-semibold">View Contracts</span>
+          <span className="mx-4 text-gray-500">|</span>
+          <span className="text-xl font-semibold">
+            <FaBell className="mr-2" />
+          </span>
         </div>
-        <SearchBar/>
-        <div className="nav-right">
-          <ul className="nav-links flex gap-5 items-center">
-            <li>
-              <NavLink
-                className="text-gray-300 font-normal hover:text-gray-400 duration-300"
-                style={NavActive}
-                end
-                to="/"
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className="text-gray-300 font-normal hover:text-gray-400 duration-300"
-                style={NavActive}
-                to={"/products"}
-              >
-                Products
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/cart" className="relative group">
-                <span>
-                  <BsCart3 />
-                  <span className=" absolute bottom-2 text-sm left-2 bg-rose-500 h-5 w-5 flex items-center justify-center rounded-full group-hover:bg-sky-500 duration-300">
-                    {cartItems.length}
-                  </span>
-                </span>
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      </div>
     </div>
   );
 };
 
 export default Navbar;
+
+
+
