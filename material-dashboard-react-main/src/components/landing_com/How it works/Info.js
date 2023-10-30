d4da6474e5e663df509c9acb960952024c9ceeab
RudraShinde75 <rudra.shinde@somaiya.edu>
2023-10-30 23:47:22 +0530

    How it works

---
+++ material-dashboard-react-main/src/components/landing_com/How it works/Info.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import "./Info.css";
+
+function Info() {
+  return (
+    <>
+        
+        <div className="registration-box-4">
+            <div className="circle-blue">1</div>
+            <h1>Registration</h1>
+        </div>
+
+        
+        <div className="big-register">
+            <div className="registration-box-2">
+                <div className="circle-yellow">A</div>
+                <h1>Buyer</h1>
+            </div>
+            <div className="registration-box-2">
+                <div className="circle-yellow">B</div>
+                <h1>Seller</h1>
+            </div>
+        </div>
+
+        <div className="registration-box-1">
+            <div className="circle-blue">A</div>
+            <h1>Buyer</h1>
+        </div>
+
+        <div className="big-register">
+        <div className='registration-box-3'>
+            <div className="step">Step 1</div>
+            <h2>View Products</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 2</div>
+            <h2>Place Order</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 3</div>
+            <h2>Fill Details</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 4</div>
+            <h2>Complete Your Placement</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 5</div>
+            <h2>Order Placed!</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 6</div>
+            <h2>Track Order</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 7</div>
+            <h2>Order Delivered</h2>
+        </div>
+        </div>
+
+
+        <div className="registration-box-1">
+            <div className="circle-blue">B</div>
+            <h1>Seller</h1>
+        </div>
+
+        <div className="big-register">
+        <div className='registration-box-3'>
+            <div className="step">Step 1</div>
+            <h2>Doc. Registration (KYC)</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 2</div>
+            <h2>Add Product</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 3</div>
+            <h2>Verify Product</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 4</div>
+            <h2>Complete Order</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 5</div>
+            <h2>Choose Postal Service</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 6</div>
+            <h2>Form Filling(PBE+CN3)</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 7</div>
+            <h2>Print Form+Invoice</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 8</div>
+            <h2>Find Nearest DNK Office</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 9</div>
+            <h2>Delivery+Customs</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 10</div>
+            <h2>Track Package</h2>
+        </div>
+        <div className='registration-box-3'>
+            <div className="step">Step 11</div>
+            <h2>Ordered Delivered</h2>
+        </div>
+        </div>
+    </>
+  );
+}
+
+export default Info;
