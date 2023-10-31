6412bd88c3713358f1e86409266350efb90419bc
Sujal Jain <sujal16@somaiya.edu>
2023-10-31 15:56:20 +0530

    write authcontext

--- material-dashboard-react-main/src/layouts/login/login.js
+++ material-dashboard-react-main/src/layouts/login/login.js
@@ -1,1 +1,12 @@
-import React  from "react";
+import React, { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const Login = () => {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState('');
+    const { login, currentUser } = useAuth()
+    const history = useNavigate()
+}
